86f657bb6f9f314787af52784a2a6708823d098d
ian <james152136@gmail.com>
2024-02-26 09:36:51 +0800

    commit

---
+++ todoApp/src/app/services/sign-up-helper.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import {FormControl,FormGroup} from '@angular/forms'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignUpHelperService {
+  
+  constructor() { }
+
+  getfirstNameErrorMessage(control: FormControl, fieldName: string) {
+    if (control.hasError('required')) {
+      return `You must enter a ${fieldName}`;
+    }
+
+    return control.hasError('invalidFirstname') ? `Not a valid ${fieldName}` : '';
+  }
+  getlastNameErrorMessage(control: FormControl, fieldName: string){
+    if (control.hasError('required')) {
+      return `You must enter a ${fieldName}`;
+    }
+
+    return control.hasError('invalidLastname') ? `Not a valid ${fieldName}` : '';
+  }
+  //email
+  getErrorMessage(email: FormControl, fieldName: string){
+    if (email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return email.hasError('email') ? 'Not a valid email' : '';
+  }
+  //password
+  checkPasswords(group: FormGroup): any {
+    const passwordControl = group.get('password');
+    const confirmPasswordControl = group.get('confirmPassword');
+
+    if (!passwordControl || !confirmPasswordControl) {
+      // Handle the case where controls are null (optional)
+      return { mismatch: true };
+    }
+
+    const password = passwordControl.value;
+    const confirmPassword = confirmPasswordControl.value;
+
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+}
--- todoApp/src/app/sign-up/sign-up.component.ts
+++ todoApp/src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder,FormGroup, FormControl, Validators} from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { SignUpHelperService } from '../services/sign-up-helper.service';
+
+
 
 
 @Component({
@@ -12,71 +14,41 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 export class SignUpComponent {
   //x button
-  showForm : boolean = true;
+  showForm: boolean = true;
   //password eye
-  hide:boolean = true;
+  hide: boolean = true;
   //confirmPass
-  
-  passwordMatchError: boolean =false;
-
   firstname = new FormControl('', [Validators.required, Validators.pattern(/^([A-Za-z]+\s?)+$/)]);
   lastname = new FormControl('', [Validators.required, Validators.pattern(/^([A-Za-z]+\s?)+$/)]);
   email = new FormControl('', [Validators.required, Validators.email]);
-  passwordForm: FormGroup = this.formBuilder.group({
-    password: ['', [Validators.required]],
-    confirmPassword: ['', [Validators.required]]
-  }, { validator: this.checkPasswords });
-  
-  
-  
-constructor(private formBuilder:FormBuilder, private snackBar: MatSnackBar ){
-  
-}
+  passwordForm: FormGroup ; 
 
-
-
-  getfirstNameErrorMessage(control: FormControl, fieldName: string) {
-    if (control.hasError('required')) {
-      return `You must enter a ${fieldName}`;
-    }
-
-    return control.hasError('invalidFirstname') ? `Not a valid ${fieldName}` : '';
+  constructor(private formBuilder: FormBuilder, private signUpHelperService: SignUpHelperService) {
+    this.passwordForm = this.formBuilder.group({
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validator: this.checkPasswords });
+   }
+  
+  firstNameErrorMessage() {
+    return this.signUpHelperService.getfirstNameErrorMessage(this.firstname, 'first Name');
   }
-  getlastNameErrorMessage(control: FormControl, fieldName: string) {
-    if (control.hasError('required')) {
-      return `You must enter a ${fieldName}`;
-    }
-
-    return control.hasError('invalidLastname') ? `Not a valid ${fieldName}` : '';
+  getlastNameErrorMessage() {
+    return this.signUpHelperService.getlastNameErrorMessage(this.lastname, 'lastname');
   }
-  
-  
   getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
+    return this.signUpHelperService.getErrorMessage(this.email, 'email');
+  }
+  checkPasswords(): any {
+    return this.signUpHelperService.checkPasswords(this.passwordForm);
+   
+  }
+  
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-  checkPasswords(group: FormGroup): any {
-    const passwordControl = group.get('password');
-    const confirmPasswordControl = group.get('confirmPassword');
-  
-    if (!passwordControl || !confirmPasswordControl) {
-      // Handle the case where controls are null (optional)
-      return { mismatch: true };
-    }
-  
-    const password = passwordControl.value;
-    const confirmPassword = confirmPasswordControl.value;
-  
-    return password === confirmPassword ? null : { mismatch: true };
-  }
-  
   onSubmit(): void {
     
   }
-  closeForm(){
+  closeForm() {
     this.showForm = false;
   }
 }
